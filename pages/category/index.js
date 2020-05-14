9ec853403b2330072d70b1d436e26d0718f8679f
liuchao <Cj180534>
2020-05-14 18:20:19 +0800

    首页和分类页面完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,66 +1,77 @@
-// pages/category/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    leftMenuList: [],
+    rightContent: [],
+    currentIndex: 0,
+    scrollTop: 0
   },
-
+  // 接口返回的数据
+  Cates: [],
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    const Cates = wx.getStorageSync("cates");
+    if (!Cates) {
+      this.getCates();
+    } else {
+      if (Date.now() - Cates.time > 1000 * 10) {
+        this.getCates();
+      } else {
+        this.Cates = Cates.data;
+        let leftMenuList = this.Cates.map(v => v.cat_name);
+        let rightContent = this.Cates[0].children;
 
+        this.setData({
+          leftMenuList,
+          rightContent
+        })
+      }
+    }
   },
+  // 获取分类数据
+  async getCates() {
+    // request({
+    //   url: '/categories'
+    // }).then(result => {
+    //   this.Cates = result.data.message;
+    //   wx.setStorageSync("cates", { time: Date.now(), data: this.Cates })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+    //   let leftMenuList = this.Cates.map(v => v.cat_name);
+    //   let rightContent = this.Cates[0].children;
 
+    //   this.setData({
+    //     leftMenuList,
+    //     rightContent
+    //   })
+    // });
+
+    const result = await request({ url: '/categories' });
+    this.Cates = result;
+    wx.setStorageSync("cates", { time: Date.now(), data: this.Cates })
+
+    let leftMenuList = this.Cates.map(v => v.cat_name);
+    let rightContent = this.Cates[0].children;
+
+    this.setData({
+      leftMenuList,
+      rightContent
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //左侧菜单点击
+  handleItemTap(e) {
+    const { index } = e.currentTarget.dataset;
+    let rightContent = this.Cates[index].children;
+    this.setData({
+      currentIndex: index,
+      rightContent,
+      scrollTop: 0
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,19 +1,47 @@
-//index.js
-//获取应用实例
-const app = getApp()
+import { request } from '../../request/index.js'
 
 Page({
   data: {
-    swiperList: []
+    // 轮播图
+    swiperList: [],
+    // 分类
+    catesList: [],
+    // 楼层
+    floorList: []
   },
   onLoad: function (ops) {
-    var reqTask = wx.request({
-      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
-      success: (result) => {
-        this.setData({
-          swiperList: result.data.message
-        })
-      }
+    this.getSwiperList();
+    this.getCatesList();
+    this.getFloorList();
+  },
+  // 获取轮播图
+  getSwiperList() {
+    request({
+      url: '/home/swiperdata'
+    }).then(result => {
+      this.setData({
+        swiperList: result
+      })
+    });
+  },
+  // 获取分类导航
+  getCatesList() {
+    request({
+      url: '/home/catitems'
+    }).then(result => {
+      this.setData({
+        catesList: result
+      })
+    });
+  },
+  // 楼层
+  getFloorList() {
+    request({
+      url: '/home/floordata'
+    }).then(result => {
+      this.setData({
+        floorList: result
+      })
     });
   }
 })
