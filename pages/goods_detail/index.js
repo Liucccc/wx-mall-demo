1f283d4da13a3f19afdad488505176f17d91966a
liuchao <Cj180534>
2020-05-15 18:10:09 +0800

    商品列表、详情

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,61 @@
-// pages/goods_detail/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj: {}
   },
+  GoodsInfo: {},
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    const { goods_id } = options;
+    this.getGoodsDetail(goods_id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 商品详情
+  async getGoodsDetail(goods_id) {
+    const res = await request({ url: "/goods/detail", data: { goods_id } })
+    this.GoodsInfo = res;
+    this.setData({
+      goodsObj: {
+        pics: res.pics,
+        goods_price: res.goods_price,
+        goods_name: res.goods_name,
+        goods_introduce: res.goods_introduce.replace(/\.webp/g, '.jpg')
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  // 轮播图点击查看大图
+  handlePreviewImage(e) {
+    const urls = this.GoodsInfo.pics.map(v => v.pics_mid);
+    let current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    });
   },
+  // 添加到购物车
+  handleCartAdd() {
+    let cart = wx.getStorageSync("cart") || [];
+    let index = cart.findIndex(v => v.goods_id === this.GoodsInfo.goods_id);
+    if (index === -1) {
+      this.GoodsInfo.num = 1;
+      cart.push(this.GoodsInfo);
+    } else {
+      cart[index].num++;
+    }
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+    wx.setStorageSync("cart", cart);
 
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    wx.showToast({
+      title: '加入成功',
+      icon: 'success',
+      mask: true
+    });
   }
 })
--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,5 @@
-// pages/goods_list/index.js
+import { request } from '../../request/index.js'
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
@@ -19,15 +20,26 @@ Page({
         value: "价格",
         isActive: false
       }
-    ]
+    ],
+    goodsList: []
   },
-
+  // 查询参数
+  QueryParams: {
+    query: "",
+    cid: "",
+    pagenum: 1,
+    pagesize: 10
+  },
+  // 总页数
+  totalPages: 1,
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.QueryParams.cid = options.cid;
+    this.getGoodsList();
   },
+  // 筛选条件点击
   handleTabsItemChange(e) {
     const { index } = e.detail;
     let { tabs } = this.data;
@@ -35,5 +47,34 @@ Page({
     this.setData({
       tabs
     })
+  },
+  // 获得商品列表
+  async getGoodsList() {
+    const res = await request({ url: "/goods/search", data: this.QueryParams })
+    const total = res.total;
+    this.totalPages = Math.ceil(total / this.QueryParams.pagesize);
+    this.setData({
+      goodsList: [...this.data.goodsList, ...res.goods]
+    })
+    wx.stopPullDownRefresh();
+  },
+  // 页面触底
+  onReachBottom() {
+    if (this.QueryParams.pagenum >= this.totalPages) {
+      wx.showToast({
+        title: '没有下一页数据了'
+      });
+    } else {
+      this.QueryParams.pagenum++;
+      this.getGoodsList();
+    }
+  },
+  // 下拉刷新
+  onPullDownRefresh() {
+    this.setData({
+      goodsList: []
+    })
+    this.QueryParams.pagenum = 1;
+    this.getGoodsList();
   }
 })
